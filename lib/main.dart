286149c481b73fbc8f71e737eecc1f2cdbdd81c9
Krinal <krinalnakarani1774@gmail.com>
2023-01-03 08:51:23 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:otpgenerator/code.dart';
+
+void main()
+{
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: code(),
+  ),
+  );
+}
